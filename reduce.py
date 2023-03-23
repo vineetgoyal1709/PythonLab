4d3676d03cdd2fa562038cc0855c506f492a257b
vineetgoyal1709 <126063939+vineetgoyal1709@users.noreply.github.com>
2023-03-23 15:10:07 +0530

    Update reduce.py

--- reduce.py
+++ reduce.py
@@ -1,16 +1,16 @@
-import operator  # importing operator
-from functools import reduce # importing reduce
-from itertools import accumulate # importing accumulate
+import operator  
+from functools import reduce 
+from itertools import accumulate 
 
 
 def sumOfList(a, b):
-    sum = a + b  # sum of a and b
+    sum = a + b  
     return sum
 
 
 List = [1, 2, 3, 4, 5, 6]
-a = (reduce(sumOfList, List))  # returns last value of the function
+a = (reduce(sumOfList, List))  
 print(a)
 
-b = list(accumulate(List, operator.add))  # returns result after every operation
+b = list(accumulate(List, operator.add)) 
 print(b)
