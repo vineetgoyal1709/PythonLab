ca95299634f279b60332a700a98f104505faf522
vineetgoyal1709 <126063939+vineetgoyal1709@users.noreply.github.com>
2023-03-23 15:05:14 +0530

    Update python_num.py

--- python_num.py
+++ python_num.py
@@ -1,14 +1,10 @@
 import numpy as np
 
-arr = np.array([[1, 2, 3], [4, 5, 6]])  # To create an array in numpy
-print(arr)  # to print the array
-print(type(arr))  # to print the type of arr created
+arr = np.array([[1, 2, 3], [4, 5, 6]])  
+print(arr)  
+print(type(arr)) 
 print()
-
-#  To print the number of element in the array:
 print("Number of elements in array :", np.size(arr))
-
-#  To print dimensions of array :
 print("Dimensions of array are :", np.shape(arr))
 
 
