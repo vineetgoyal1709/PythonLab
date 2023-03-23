8c1f811142606606039a860510f08f0d59cfb683
vineetgoyal1709 <126063939+vineetgoyal1709@users.noreply.github.com>
2023-03-23 14:57:37 +0530

    Update lambdaFunc.py

--- lambdaFunc.py
+++ lambdaFunc.py
@@ -1,13 +1,9 @@
-x = lambda a: a + 10  # giving only single argument
+x = lambda a: a + 10 
 print("Adding 10 to a : ", x(5))
 
-x = lambda a, b: a * b  # giving two argument a and b
-print("Multiplying two numbers", x(4, 5))  # giving the value in function
-
-
-def double(n):  # function to multiply the number N times
-    return lambda a: a * n  # a times n
-
-
-func = double(2)  # multiplying number 2, a times
-print(func(4))  # giving number of times to multiply
+x = lambda a, b: a * b
+print("Multiplying two numbers", x(4, 5))  
+def double(n):  
+    return lambda a: a * n 
+func = double(2)  
+print(func(4))  
