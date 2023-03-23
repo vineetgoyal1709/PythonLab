4b33dc3d98c0f8568f0c5f058d06668bc88a7d6a
vineetgoyal1709 <126063939+vineetgoyal1709@users.noreply.github.com>
2023-03-23 14:36:31 +0530

    Update date_time.py

--- date_time.py
+++ date_time.py
@@ -1,44 +1,29 @@
 import datetime
 from datetime import date
-
-# write a python program to add some dates to the present date and print the day
-
-# to print the date(yyyy-mm-dd) and time
 now = datetime.datetime.now()
 print(now)
-
-# To print the current date(yyyy-mm-dd)
 current_date = datetime.date.today()
 print(current_date)
 
-# To print a specific date(yyyy-mm-dd)
 d = datetime.date(2023, 3, 15)
 print(d)
 
-# today() to get current date
 todays_date = date.today()
 print("Today's date =", todays_date)
 
-# timestamp = january1, 1970
-timestamp = date.fromtimestamp(77)  # adds 77 seconds to the timestamp
+timestamp = date.fromtimestamp(77) 
 print("Date =", timestamp)
-
-# to print today's year, month, day
 today = date.today()
 
 print("Current year:", today.year)
 print("Current month:", today.month)
 print("Current day:", today.day)
-
-# formats of date and time
 now = datetime.datetime.now()
 t = now.strftime("%H:%M:%S")
 print("Time:", t)
 s1 = now.strftime("%m/%d/%Y, %H:%M:%S")
-# mm/dd/YY H:M:S format
 print("s1:", s1)
 s2 = now.strftime("%d/%m/%Y, %H:%M:%S")
-# dd/mm/YY H:M:S format
 print("s2:", s2)
 
 # To add days to present date and tell the date:
