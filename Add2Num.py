bf0d717704ccc12cf3d5cdfb130082ff7776d93e
vineetgoyal1709 <126063939+vineetgoyal1709@users.noreply.github.com>
2023-03-23 14:09:07 +0530

    Update Add2Num.py

--- Add2Num.py
+++ Add2Num.py
@@ -22,7 +22,6 @@ def pos_neg(num):
     else:
         return "negative"
 
-#commit
 def day(num):  
     if 0 < num < 8:
         Dict = {1: "Monday",2: "Tuesday",3: "Wednesday", 4: "Thursday",5: "Friday",6: "Saturday", 7: "Sunday" }
