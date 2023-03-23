a56e83c1c640ba3b75660ef5a95c349f94e60764
vineetgoyal1709 <126063939+vineetgoyal1709@users.noreply.github.com>
2023-03-23 18:48:41 +0530

    Update tupple.py

--- tupple.py
+++ tupple.py
@@ -1,4 +1,4 @@
-Tuple = ("First index", 1, 2, 3, "Satyam", True, False, "Last index")
+Tuple = ("First index", 1, 2, 3, "aiads", True, False, "Last index")
 print("Length of tuple", len(Tuple))  
 print("Backward indexing : ", Tuple[-1])  
 print("Forward indexing : ", Tuple[0]) 
@@ -10,7 +10,7 @@ List = [New_Tuple, 1, 2, 34]
 print("nested tuple in list:", List) 
 List1 = list(New_Tuple)
 print("Changing tuple into list", List1) 
-List1[1] = "Satyam"  
+List1[1] = "aiads"  
 print("After changing value : ", List1)
 List1.append("New element")
 New_Tuple = tuple(List1)
