ffc253c126fec42316cfd696f881f37d7b01266a
vineetgoyal1709 <126063939+vineetgoyal1709@users.noreply.github.com>
2023-03-23 14:28:05 +0530

    Update calender.py

--- calender.py
+++ calender.py
@@ -1,24 +1,18 @@
-# Python program to display calendar of 2023
-# given month of the year
-
-# import module
 import calendar
 
 month = 1
 while month < 13:
-    print(calendar.month(2023, month))  # prints calendar of the month
+    print(calendar.month(2023, month)) 
     month += 1
 
 
 def year():
-    print(calendar.calendar(2023))  # prints all months of the year (in tabular form)
-
+    print(calendar.calendar(2023))
 
 def month():
-    print(calendar.month(2023, 2))  # prints the given month which is provided
-
+    print(calendar.month(2023, 2)) 
 
 def gap():
-    print(calendar.month(2023, 2, 5))  # here 5 provides the space between dates
+    print(calendar.month(2023, 2, 5))
 
 
