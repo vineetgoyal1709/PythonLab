3cb4fa37b6fbc028e4e7506a004e93f1fdc91e8d
vineetgoyal1709 <126063939+vineetgoyal1709@users.noreply.github.com>
2023-03-23 15:02:26 +0530

    Update pallindromeloop.py

--- pallindromeloop.py
+++ pallindromeloop.py
@@ -1,11 +1,11 @@
-def Palindrome(checkWord):  # userDefined function
-    for i in range(0, int(len(checkWord) / 2)):  # 0 to halfOfWord
-        if checkWord[i] != checkWord[len(checkWord) - i - 1]:  # checking 1st and last letter
+def Palindrome(checkWord): 
+    for i in range(0, int(len(checkWord) / 2)):  
+        if checkWord[i] != checkWord[len(checkWord) - i - 1]:  
             return False
     return True
 
 
-word = "maam"  # Word to check
+word = "maam" 
 result = Palindrome(word)
 
 if result:
