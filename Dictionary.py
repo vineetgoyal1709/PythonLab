fece8c6424a8f3ab19f2693b1555355b383eca4a
vineetgoyal1709 <126063939+vineetgoyal1709@users.noreply.github.com>
2023-03-23 14:24:59 +0530

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -1,29 +1,17 @@
-# Creating Dictionary:
 Dictionary = {
     "Key": "Value",
     1: "Hello",
     2: "Welcome",
 }
-
-print("Dictionary-", Dictionary)  # printing Dictionary
-
-# Changing values in Dictionary:
-Dictionary[1] = "Value Changed" #
+print("Dictionary-", Dictionary) 
+Dictionary[1] = "Value Changed"
 print("After changing value:", Dictionary)
-
-# Adding keys and values in Dictionary:
-Dictionary["New Key"] = "New Value"   #initialising key and value
+Dictionary["New Key"] = "New Value"  
 print("After adding new key", Dictionary)
-
-# Removing items from Dictionary:
-Dictionary.pop("New Key")   # using pop function to remove item
+Dictionary.pop("New Key")   
 print("Removing items", Dictionary)
-
-# Nested Dictionary:
-New_Dictionary = {1: True, 2: False}  # Creating new dictionary
-Dictionary["New Dictionary"] = New_Dictionary  # Nesting new dictionary
+New_Dictionary = {1: True, 2: False} 
+Dictionary["New Dictionary"] = New_Dictionary  
 print("Nested Dictionary", Dictionary)
-
-# Updating Dictionary using update function:
-Dictionary.update({1: "value updated"})  # updating value of key "1"
+Dictionary.update({1: "value updated"})  
 print("updated value: ", Dictionary)
