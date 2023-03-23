175e404f3b3839eb7fe9e0783c4fae30d6e2f3e4
vineetgoyal1709 <126063939+vineetgoyal1709@users.noreply.github.com>
2023-03-23 15:06:03 +0530

    Update pythonversion.py

--- pythonversion.py
+++ pythonversion.py
@@ -1,25 +1,24 @@
 import os
 
-path = "C:/Users/Hp/OneDrive/Desktop/OS.txt"  # path of the file
+path = "C:/Users/Hp/OneDrive/Desktop/OS.txt"  
 
-
-def file_exists(): # Checks whether the file exists or not at the path given
+def file_exists():
     print(os.access(path, os.F_OK))
 
 
-def file_read(): # Checks whether user can read or not
+def file_read(): 
     print(os.access(path, os.R_OK))
 
 
-def file_write():  # Checks whether there is permission to write or not
+def file_write(): 
     print(os.access(path, os.W_OK))
 
 
-def path_execution():  # Checks if path can be executed
+def path_execution(): 
     print(os.access(path, os.X_OK))
 
 
-# Gives boolean value as Output
+
 file_write()
 file_read()
 file_exists()
