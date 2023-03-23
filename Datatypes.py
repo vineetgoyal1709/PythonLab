2d0708739569f43fdffc9854503eb1b2d4a21b24
vineetgoyal1709 <126063939+vineetgoyal1709@users.noreply.github.com>
2023-03-23 14:23:29 +0530

    Update Datatypes.py

--- Datatypes.py
+++ Datatypes.py
@@ -1,14 +1,10 @@
-# Data types in Python:
-# initialising data types
-int_type = 1  # integer type
-float_type = 1.2  # float type
-string_type = "String"  # string type
-dictionary = {"Key": "Value"}  # dictionary
-List = ["element1", "element2", 1, 2, 3]  # list
-Boolean = True  # boolean type
-Tuple = (1, 2, 3, 4, "item1", "item2")  # tuple
-
-# printing types of variables:
+int_type = 1
+float_type = 1.2  
+string_type = "String"  
+dictionary = {"Key": "Value"}  
+List = ["element1", "element2", 1, 2, 3]  
+Boolean = True  
+Tuple = (1, 2, 3, 4, "item1", "item2")  
 print(type(int_type))
 print(type(float_type))
 print(type(string_type))
